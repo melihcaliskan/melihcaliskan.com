18add5645b2ce73e400a5c867e93fc4baab3f4c3
melih <mlhclskn@gmail.com>
2020-08-15 01:12:56 +0300

    Created index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import Head from 'next/head'
-import Twemoji from '../components/Twemoji';
+import React, { useState } from 'react';
 
-export default function Home() {
+import Container from '../components/Container'
+import Fade from 'react-reveal/Fade';
+import Head from 'next/head'
+import Text from '../components/Text'
+import Twemoji from '../components/Twemoji';
+import { useRouter } from 'next/router'
+
+const Home = ({ theme, toggleTheme }) => {
   return (
-    <div className="container">
-    </div>
-  )
+    <Container>
+      <Fade>
+        <Text onClick={() => toggleTheme()}>
+          Merhaba, ben <Text color={theme.body_700}>Melih Çalışkan.</Text> Dumlupınar Üniversitesi <Text color={theme.body_700}>Bilgisayar Mühendisliği</Text>  öğrencisiyim.
+        Kendi <Text underline color={theme.body_700}>girişimlerimi</Text> geliştiriyor ve <Text underline color={theme.body_700}>freelance</Text>  projelerde çalışıyorum.
+      </Text>
+      </Fade>
+    </Container>
+  );
 }
+export default Home
