7c64aa302c333d0cc9b44466ff22fd9df4e82a4e
melih <mlhclskn@gmail.com>
2020-08-15 01:11:36 +0300

    Revert "added underline and responsive support"
    This reverts commit ff68dbc9ded8daaa739c8a7f863cf2ccd36be8de.

--- components/Text.js
+++ components/Text.js
@@ -1,18 +1,7 @@
 import styled from 'styled-components'
 
 const Text = styled.h1`
-    display: inline;
-    color:${props => props.color ? props.color : props.theme.body_600};
-    line-height:1.25em;
-    
-    text-decoration:${props => props.underline && "underline"};
-    text-decoration-style: ${props => props.underline && "dotted"};
-
-    @media only screen and (max-width: 740px) {
-        & {
-            font-size:2rem;
-        }
-    }
+      color:${props => props.color ? props.color : props.theme.body_600};
 `
 
 const CustomText = (props) => {
