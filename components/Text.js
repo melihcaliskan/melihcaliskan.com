a810444de85ef3232a402c9a61812b0d2fd77d49
melih <mlhclskn@gmail.com>
2020-08-15 01:29:49 +0300

    updated text size

--- components/Text.js
+++ components/Text.js
@@ -9,6 +9,12 @@ const Text = styled.h1`
     text-decoration:${props => props.underline && "underline"};
     text-decoration-style: ${props => props.underline && "dotted"};
 
+    @media only screen and (max-width: 1280px) {
+        & {
+            font-size:2.8rem;
+        }
+    }
+
     @media only screen and (max-width: 740px) {
         & {
             font-size:2rem;
