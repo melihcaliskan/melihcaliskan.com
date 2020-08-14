07badcaaace9394974fa595dbc7323408f97e329
melih <mlhclskn@gmail.com>
2020-08-15 00:45:32 +0300

    Create global.js

---
+++ assets/global.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+    transition: all 0.15s linear;
+  }
+
+  a,p,span {
+    color: ${({ theme }) => theme.text};
+  }
+
+  ::-webkit-scrollbar {
+    border-radius: 10px;
+    background-color: ${({ theme }) => theme.scrollbar_light};
+    height:15px;
+  }
+
+  ::-webkit-scrollbar-track {
+    border-radius: 10px;
+    background-color: ${({ theme }) => theme.scrollbar_light};
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-color: ${({ theme }) => theme.scrollbar_dark};
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    border-radius: 10px;
+    background-color: ${({ theme }) => theme.scrollbar_hover};
+  }
+`
