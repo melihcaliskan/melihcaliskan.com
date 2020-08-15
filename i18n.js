8c57c3d9d27d45d59f41fd3c37eb2eae114b8407
melih <mlhclskn@gmail.com>
2020-08-15 17:13:48 +0300

    Create i18n.js

---
+++ i18n.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const NextI18Next = require('next-i18next').default
+const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
+
+module.exports = new NextI18Next({
+    otherLanguages: ['en', 'de'],
+    localeSubpaths,
+    localePath: path.resolve('./public/static/locales'),
+})
