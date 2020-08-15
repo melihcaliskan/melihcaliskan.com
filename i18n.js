df4701c5aadb37d5c946a627b77a30f6d71533da
melih <mlhclskn@gmail.com>
2020-08-15 19:19:54 +0300

    Update i18n.js

--- i18n.js
+++ i18n.js
@@ -1,9 +1,7 @@
 const path = require('path')
 const NextI18Next = require('next-i18next').default
-const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
 
 module.exports = new NextI18Next({
   otherLanguages: ['tr', 'de'],
-  localeSubpaths,
   localePath: path.resolve('./public/static/locales'),
 })
