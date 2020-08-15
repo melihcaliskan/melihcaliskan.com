4aeb363ac108987b394da3e902b94938163fdd5a
melih <mlhclskn@gmail.com>
2020-08-15 19:58:49 +0300

    updated dropdown size

--- components/Container.js
+++ components/Container.js
@@ -7,30 +7,33 @@ const Container = styled.div`
 
 
     .svg-inline--fa{
-        width:30px;
-        height:30px;
+        width:36px;
+        height:36px;
     }
 
     @media only screen and (max-width: 1280px) {
+        .svg-inline--fa{
+            width:28px;
+            height:28px;
+        }
         & {
             line-height:60px;
         }
     }
 
     @media only screen and (max-width: 960px), (max-height: 770px) {   
-        .svg-inline--fa{
-            width:20px;
-            height:20px;
-            margin-right:-0.3em;
-        }
-
+        
         & {
             margin:3vh auto;
             padding:2em;
         }
     }
     
-    @media only screen and (max-width: 740px) {        
+    @media only screen and (max-width: 740px) {  
+        .svg-inline--fa{
+            width:24px;
+            height:24px;
+        }      
         & {
             line-height:40px;
         }
