8226c5c8a8da42b8d0f88b55d3bdae3560e5982e
melih <mlhclskn@gmail.com>
2020-08-15 19:54:00 +0300

    revert toggle

--- pages/index.js
+++ pages/index.js
@@ -11,24 +11,18 @@ import handleLanguage from '../helpers/handleLanguage'
 import styled from 'styled-components'
 import { useRouter } from 'next/router'
 
-const LanguageSelect = styled.select`
-  width:auto;
-  border:0;
-  background:transparent;
-  display:inline;
-  font-size:64px;
-  font-weight:700;
-  color:${({ theme }) => theme.body_600};
-
-  & optgroup { 
-    font-size:10px;
-  }
-
+const LanguageToggle = styled.button`
+    cursor:pointer;
+    border:0;
+    margin-left:-5px;
+    background:transparent;
+    font-weight:700;
+    color:${({ theme }) => theme.body_600};
 `
 
 const Turkish = ({ t, theme }) => {
   return (
-    <Text>, {t('iam')} <Text color={theme.body_700}>{t('name')}.</Text> {t('university')} <Text color={theme.body_700}>{t('faculty')}</Text> {t('student')}.
+    <Text>{t('iam')} <Text color={theme.body_700}>{t('name')}.</Text> {t('university')} <Text color={theme.body_700}>{t('faculty')}</Text> {t('student')}.
       {' ' + t('myown')} <Text underline color={theme.body_700}>{t('startup')}</Text> {t('workon')} <Text underline color={theme.body_700}>{t('freelance')}</Text>  {t('projects')}
     </Text>
   )
@@ -36,7 +30,7 @@ const Turkish = ({ t, theme }) => {
 
 const English = ({ t, theme }) => {
   return (
-    <Text>, {t('iam')} <Text color={theme.body_700}>{t('name')}.
+    <Text>{t('iam')} <Text color={theme.body_700}>{t('name')}.
       <br />
       {t('student')} </Text> {t('university')}, <Text color={theme.body_700}>{t('faculty')}</Text>.
       {' ' + t('myown')} <Text underline color={theme.body_700}>{t('startup')}</Text> {t('workon')} <Text underline color={theme.body_700}>{t('freelance')}</Text>  {t('projects')}
@@ -45,32 +39,15 @@ const English = ({ t, theme }) => {
 }
 const Deutsch = ({ t, theme }) => {
   return (
-    <Text>, {t('iam')} <Text color={theme.body_700}>{t('name')}.
+    <Text>{t('iam')} <Text color={theme.body_700}>{t('name')}.
       <br />
       {t('student')} </Text> {t('university')}, <Text color={theme.body_700}>{t('faculty')}</Text>.
       {' ' + t('myown')} <Text underline color={theme.body_700}>{t('startup')}</Text> {t('workon')} <Text underline color={theme.body_700}>{t('freelance')}</Text>  {t('projects')}
     </Text>
   )
 }
-
-const languageList = [
-  {
-    name: "Merhaba",
-    value: "tr",
-  },
-  {
-    name: "Hello",
-    value: "en",
-  },
-  {
-    name: "Hallo",
-    value: "de",
-  },
-]
-
 const Home = ({ loading, t, isLight, theme, toggleTheme }) => {
-  const [language, setLanguage] = useState(i18n.language);
-
+  const language = i18n.language
   const handleLoad = () => {
     // WIP
     const text = "loading"
@@ -87,30 +64,30 @@ const Home = ({ loading, t, isLight, theme, toggleTheme }) => {
   }
 
   return (
-    <Fade>
-      <Container>
+    <Container>
+      <Fade>
         <ThemeToggle isLight={isLight} theme={theme} toggleTheme={toggleTheme} />
-        <LanguageSelect onChange={(e) => handleLanguage(e)} value={i18n.language}>
-          <optgroup>
-            {languageList.map(item =>
-              <option key={item.value} value={item.value}>
-                {item.name}
-              </option>
-
-            )}
-          </optgroup>
-        </LanguageSelect>
-        {language == "tr" ?
-          <Turkish t={t} theme={theme} />
-          :
-          language == "en" ?
-            <English t={t} theme={theme} />
+        <Text>
+          <LanguageToggle
+            title={t('changelanguage')}
+            type='button'
+            onClick={() => handleLanguage()}>
+            {t('hello')}
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="caret-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="svg-inline--fa fa-caret-down fa-w-10 fa-2x"><path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z" class=""></path></svg>
+          </LanguageToggle>
+          {", "}
+          {language == "tr" ?
+            <Turkish t={t} theme={theme} />
             :
-            language == "de" &&
-            <Deutsch t={t} theme={theme} />
-        }
-      </Container>
-    </Fade>
+            language == "en" ?
+              <English t={t} theme={theme} />
+              :
+              language == "de" &&
+              <Deutsch t={t} theme={theme} />
+          }
+        </Text>
+      </Fade>
+    </Container>
   );
 }
 
