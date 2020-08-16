a2f6c68c0126ace8a6806685f74cb3159b12d529
melih <mlhclskn@gmail.com>
2020-08-16 16:13:22 +0300

    updated loader font-size

--- pages/index.js
+++ pages/index.js
@@ -164,7 +164,7 @@ const Home = (props) => {
 
   if (loading) {
     return (
-      <div style={{ color: '#464646', fontFamily: "-apple-system, BlinkMacSystemFont, \"Segoe UI\", Roboto, \"Helvetica Neue\", Arial, sans-serif", fontWeight: 700, position: "absolute", top: "50%", left: "50%", marginRight: "-50%", transform: "translate(-50%, -50%)", animation: "fadeIn ease .2s", WebkitAnimation: "fadeIn ease .2s", MozAnimation: "fadeIn ease .2s" }}><h1>Loading...</h1></div>
+      <div style={{ fontSize: '48px', color: '#464646', fontFamily: "-apple-system, BlinkMacSystemFont, \"Segoe UI\", Roboto, \"Helvetica Neue\", Arial, sans-serif", fontWeight: 700, position: "absolute", top: "50%", left: "50%", marginRight: "-50%", transform: "translate(-50%, -50%)", animation: "fadeIn ease .2s", WebkitAnimation: "fadeIn ease .2s", MozAnimation: "fadeIn ease .2s" }}><h1>Loading...</h1></div>
     )
   }
 
