00dd979339232f47361786e9a50cf44f1387aec0
Melih Çalışkan <mlhclskn@gmail.com>
2023-07-02 12:11:35 +0300

    Responsive improvements

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -2,10 +2,17 @@ import { HNC_URL } from "@/utils/constants";
 
 export const HeroText = () => {
   return (
-    <h1 className="select-none leading-normal mt-16 text-4xl font-extrabold text-gray-500 dark:text-gray-400 md:text-5xl md:font-bold md:leading-[4rem] lg:text-6xl lg:font-bold lg:leading-[4.5rem]">
-      Hi 👋🏻,&nbsp;I’m <span className="hover:text-gray-700 dark:text-gray-200 dark:hover:text-white transition-colors ease-linear">Melih Çalışkan</span>
-      , a <span className="hover:text-gray-700 dark:text-gray-200 dark:hover:text-white transition-colors ease-linear">Software Developer</span> working
-      at{" "}
+    <h1 className="select-none leading-normal mt-16 mb-4 text-4xl font-extrabold text-gray-500 dark:text-gray-400 text-[2rem] md:text-5xl md:font-bold md:leading-[4rem] lg:text-6xl lg:font-bold lg:leading-[4.5rem]">
+      <p className="lg:inline">Hi 👋🏻,&nbsp;</p>
+      I’m{" "}
+      <span className="hover:text-gray-700 dark:text-gray-200 dark:hover:text-white transition-colors ease-linear">
+        Melih Çalışkan
+      </span>
+      , a{" "}
+      <span className="hover:text-gray-700 dark:text-gray-200 dark:hover:text-white transition-colors ease-linear">
+        Software Developer
+      </span>{" "}
+      working at{" "}
       <a
         href={HNC_URL}
         target="_blank"
