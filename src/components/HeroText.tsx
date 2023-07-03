4bb58383458a8691037e8f637df44c3764310846
Melih Çalışkan <mlhclskn@gmail.com>
2023-07-03 23:08:43 +0300

    Text color changes

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -5,18 +5,18 @@ export const HeroText = () => {
     <h1 className="select-none leading-normal mt-16 mb-4 text-4xl font-extrabold text-gray-500 dark:text-gray-400 text-[2rem] md:text-5xl md:font-bold md:leading-[4rem] lg:text-6xl lg:font-bold lg:leading-[4.5rem]">
       <p className="lg:inline">Hi 👋🏻,&nbsp;</p>
       I’m{" "}
-      <span className="hover:text-gray-700 dark:text-gray-200 dark:hover:text-white transition-colors ease-linear">
+      <span className="text-gray-700 hover:text-black dark:text-gray-200 dark:hover:text-white transition-colors ease-linear">
         Melih Çalışkan
       </span>
       , a{" "}
-      <span className="hover:text-gray-700 dark:text-gray-200 dark:hover:text-white transition-colors ease-linear">
+      <span className="text-gray-700 hover:text-black dark:text-gray-200 dark:hover:text-white transition-colors ease-linear">
         Software Developer
       </span>{" "}
       working at{" "}
       <a
         href={HNC_URL}
         target="_blank"
-        className="hover:text-gray-700 dark:text-gray-200 dark:hover:text-white transition-colors ease-linear underline underline-offset-8 decoration-dotted"
+        className="text-gray-700 hover:text-black dark:text-gray-200 dark:hover:text-white transition-colors ease-linear underline underline-offset-8 decoration-dotted"
       >
         Hack and Craft
       </a>
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -9,7 +9,7 @@ export default function Text({ href, children }: ITextProps) {
     <a
       href={href}
       target="_blank"
-      className="block leading-normal text-4xl font-extrabold text-gray-500 dark:text-gray-400 md:text-5xl md:font-bold md:leading-[4rem] lg:text-6xl lg:font-bold lg:leading-[4.5rem]"
+      className="block leading-normal text-4xl font-extrabold text-gray-600 dark:text-gray-300 md:text-5xl md:font-bold md:leading-[4rem] lg:text-6xl lg:font-bold lg:leading-[4.5rem]"
     >
       {children}
     </a>
