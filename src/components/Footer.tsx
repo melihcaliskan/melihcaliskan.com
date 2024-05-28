833906de6062be39ab7d9c76f7d3c467bad79d45
Melih Çalışkan <mlhclskn@gmail.com>
2024-05-28 19:03:03 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,13 @@ import { HeroText } from "@/components/HeroText";
 import MenuToggle from "@/components/MenuToggle";
 import Image from "next/image";
 
+const d = new Date();
+const year = d.getFullYear();
+
 export default function Footer() {
   return (
     <footer className="z-50 py-6 border-solid border-t border-gray-200 dark:border-gray-800 max-h-[8vh]">
-      <p className="transition-colors	ease-linear text-center text-gray-400 hover:text-gray-700	dark:text-gray-600 dark:hover:text-gray-300">2023 | @melih_caliskan</p>
+      <p className="transition-colors	ease-linear text-center text-gray-400 hover:text-gray-700	dark:text-gray-600 dark:hover:text-gray-300">{year} | @melih_caliskan</p>
     </footer>
   );
 }
